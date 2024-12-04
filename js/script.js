ada7c6c0294d766fd50af13b6175693cddc5ed79
Đang chill 📻‍ <huunhat07@gmail.com>
2024-12-05 01:02:08 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,1 @@
-var clipboard1=new ClipboardJS('.main-button.copy');clipboard1.on('success',function(e)
-{yaCounter46328574.reachGoal('copygta5');e.trigger.innerHTML="Скопировано";});
+var clipboard1 = new ClipboardJS('.main-button.copy'); clipboard1.on('success', function (e) { yaCounter46328574.reachGoal('copygta5'); e.trigger.innerHTML = "Đã sao chép"; });
